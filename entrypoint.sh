6711a7d07a575f43d558f69c5fbe32ea716f9140
viktor-isaev <33855398+viktor-isaev@users.noreply.github.com>
2017-12-07 15:17:12 +0300

    Fixed issue with .dmp import.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 # Prevent owner issues on mounted folders
 chown -R oracle:dba /u01/app/oracle
+chown -R oracle:dba /docker-entrypoint-initdb.d/
 rm -f /u01/app/oracle/product
 ln -s /u01/app/oracle-product /u01/app/oracle/product
 
